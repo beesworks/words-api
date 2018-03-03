7c2530af401637279911e94cfcab69027fcad62e
Medhat Dawoud <medhatdawoud@gmail.com>
2018-03-04 00:33:52 +0100

    Use the heruku db on local as well

--- mongo.config.js
+++ mongo.config.js
@@ -5,7 +5,8 @@ mongoose.Promise = require('bluebird');
 if (process.env.NODE_ENV === 'production') {
     mongoose.connect('mongodb://heroku_m57t1s62:cc4bcejjtvbrk0pe51dg42kmsd@ds035975.mlab.com:35975/heroku_m57t1s62');
 } else {
-    mongoose.connect('mongodb://localhost/words');
+    mongoose.connect('mongodb://heroku_m57t1s62:cc4bcejjtvbrk0pe51dg42kmsd@ds035975.mlab.com:35975/heroku_m57t1s62');
+    // mongoose.connect('mongodb://localhost/words');
 }
 
 var db = mongoose.connection;
