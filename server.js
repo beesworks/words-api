4e991024d012caa411e2168db13814860370c448
Medhat Dawoud <medhatdawoud@gmail.com>
2018-03-11 15:43:41 +0100

    Support cors

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 'use strict';
 
-var express     = require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var port        = process.env.PORT || 8080;
-var path        = require('path');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 8080;
+var path = require('path');
+var cors = require('cors');
 
-const {schema} = require('./app/graphql/graphql-schema');
+const { schema } = require('./app/graphql/graphql-schema');
 const graphqlHTTP = require('express-graphql');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', cors(), graphqlHTTP({
   schema,
   graphiql: true,
 }))
